0e895ae3cb31ca0e89cd9e8df6e77a0ae36c2eaf
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2017-02-07 11:39:33 +0530

    Add three binding examples

---
+++ app/components/binding-example-3.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  russianAuthors: ['Dostoievsky', 'Tolstoi'],
+  frenchAuthors: ['Dumas', 'Moliére', 'Descartes'],
+  guatemalaAuthors: ['Vargas', 'Pazos', 'Hernández']
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('templates', function() {
+    this.route('1-sweet-templates');
+  });
 });
 
 export default Router;
